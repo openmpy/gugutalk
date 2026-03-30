1fa91c538c5e37033adb209791b1c146559334eb
openmpy <suhwan@kakao.com>
2026-03-31 05:27:39 +0900

    refactor: AuthService에 baseURL 추가해 회원가입 API 주소 정리

--- app/app/Service/Auth/AuthService.swift
+++ app/app/Service/Auth/AuthService.swift
@@ -5,11 +5,12 @@ final class AuthService {
     static let shared = AuthService()
 
     let session = Session(interceptor: AuthInterceptor())
+    let baseURL = "http://192.168.0.15:8080/api"
 
     func sendCodeVerificationCode(
         phoneNumber: String
     ) async throws {
-        let url = "http://192.168.0.15:8080/api/v1/auth/phone/send?phoneNumber=\(phoneNumber)"
+        let url = "\(baseURL)/v1/auth/phone/send?phoneNumber=\(phoneNumber)"
 
         return try await AF.request(
             url,
@@ -25,7 +26,7 @@ final class AuthService {
         password: String,
         gender: String
     ) async throws -> SignupResponse {
-        let url = "http://192.168.0.15:8080/api/v1/auth/signup"
+        let url = "\(baseURL)/v1/auth/signup"
         let body = SignupRequest(
             uuid: uuid,
             phoneNumber: phoneNumber,
