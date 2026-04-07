cadd0c363ed20c55e9553fbc6295e86abf299a4e
openmpy <suhwan@kakao.com>
2026-04-08 00:35:40 +0900

    feat: WebMvcConfig에 환경설정 기반 CORS 매핑을 추가

--- server/src/main/kotlin/com/pidulgi/server/common/config/WebMvcConfig.kt
+++ server/src/main/kotlin/com/pidulgi/server/common/config/WebMvcConfig.kt
@@ -2,18 +2,47 @@ package com.pidulgi.server.common.config
 
 import com.pidulgi.server.common.auth.AuthenticationPrincipalArgumentResolver
 import com.pidulgi.server.common.auth.JwtProvider
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebMvcConfig(
+
+    @Value("\${cors.path-pattern}")
+    private val pathPattern: String,
+
+    @Value("\${cors.origins}")
+    private val allowedOrigins: Array<String>,
+
+    @Value("\${cors.methods}")
+    private val allowedMethods: Array<String>,
+
+    @Value("\${cors.headers}")
+    private val allowedHeaders: Array<String>,
+
+    @Value("\${cors.allow-credentials}")
+    private val allowCredentials: Boolean,
+
+    @Value("\${cors.max-age}")
+    private val maxAge: Long,
 
     private val jwtProvider: JwtProvider,
 ) : WebMvcConfigurer {
 
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(argumentResolver())
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping(pathPattern)
+            .allowedOrigins(*allowedOrigins)
+            .allowedMethods(*allowedMethods)
+            .allowedHeaders(*allowedHeaders)
+            .allowCredentials(allowCredentials)
+            .maxAge(maxAge)
     }
 
     private fun argumentResolver(): AuthenticationPrincipalArgumentResolver {
