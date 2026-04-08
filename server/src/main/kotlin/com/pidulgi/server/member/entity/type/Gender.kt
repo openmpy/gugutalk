603f5c503379f4cbee28d9467b5c19525fe66e0e
openmpy <suhwan@kakao.com>
2026-04-09 01:55:00 +0900

    feat: 회원 성별 타입과 유효성 검증 테스트를 추가

--- server/src/main/kotlin/com/pidulgi/server/member/entity/type/Gender.kt
+++ server/src/main/kotlin/com/pidulgi/server/member/entity/type/Gender.kt
@@ -1,6 +1,18 @@
 package com.pidulgi.server.member.entity.type
 
+import com.pidulgi.server.common.exception.CustomException
+
 enum class Gender {
 
-    MALE, FEMALE
+    MALE, FEMALE;
+
+    companion object {
+        fun from(value: String): Gender {
+            return try {
+                valueOf(value.uppercase())
+            } catch (_: IllegalArgumentException) {
+                throw CustomException("성별이 유효하지 않습니다. (입력: $value)")
+            }
+        }
+    }
 }
