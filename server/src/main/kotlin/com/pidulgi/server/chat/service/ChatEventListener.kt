11a709bc47b1c20021dd55028853c174239e9830
openmpy <suhwan@kakao.com>
2026-04-12 00:09:01 +0900

    chore: 채팅방 삭제 이벤트 브로드캐스트 로직에서 불필요 의존성 제거

--- server/src/main/kotlin/com/pidulgi/server/chat/service/ChatEventListener.kt
+++ server/src/main/kotlin/com/pidulgi/server/chat/service/ChatEventListener.kt
@@ -3,7 +3,6 @@ package com.pidulgi.server.chat.service
 import com.pidulgi.server.chat.dto.event.ChatEvent
 import com.pidulgi.server.chat.dto.event.ChatRoomDeleteEvent
 import com.pidulgi.server.chat.dto.event.type.ChatEventType.DELETE_CHAT_ROOM
-import com.pidulgi.server.chat.repository.ChatRoomRepository
 import com.pidulgi.server.chat.service.event.ChatDeleteEvent
 import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Component
@@ -13,12 +12,12 @@ import org.springframework.transaction.event.TransactionalEventListener
 @Component
 class ChatEventListener(
 
-    private val chatRoomRepository: ChatRoomRepository,
     private val messagingTemplate: SimpMessagingTemplate,
 ) {
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     fun deleteChatRoom(event: ChatDeleteEvent) {
+        // 전체
         val chatEvent = ChatEvent(
             DELETE_CHAT_ROOM,
             null
@@ -28,7 +27,7 @@ class ChatEventListener(
             chatEvent
         )
 
-        // 채널 구독 개인 전송
+        // 개인
         val chatRoomEvent = ChatEvent(
             DELETE_CHAT_ROOM,
             ChatRoomDeleteEvent(
